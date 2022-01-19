6285f11448f6564bbc09048ec204c29cd94e3931
unknown <mriv8130@interponce.edu>
2022-01-19 03:24:40 -0400

    updta

--- src/App.js
+++ src/App.js
@@ -247,16 +247,7 @@ function App() {
                 textAlign: "center",
               }}
             >
-              <StyledButton
-                onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
-                }}
-                style={{
-                  margin: "5px",
-                }}
-              >
-                Roadmap
-              </StyledButton>
+
               <StyledButton
                 style={{
                   margin: "5px",
